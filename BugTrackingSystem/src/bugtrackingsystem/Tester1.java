6f766a3658a59dd8c6b436f17399638a5a71a813
MARIAM5524 <95701016+MARIAM5524@users.noreply.github.com>
2022-08-12 00:41:00 +0200

    solve problem of tester design

--- BugTrackingSystem/src/bugtrackingsystem/Tester1.java
+++ BugTrackingSystem/src/bugtrackingsystem/Tester1.java
@@ -28,15 +28,15 @@ public class Tester1 extends javax.swing.JFrame implements dataConnection {
         SidePanel = new javax.swing.JPanel();
         testerName = new javax.swing.JLabel();
         testerLogo = new javax.swing.JLabel();
-        testerLogo1 = new javax.swing.JLabel();
-        testerName1 = new javax.swing.JLabel();
-        testerLogo2 = new javax.swing.JLabel();
-        testerName2 = new javax.swing.JLabel();
-        testerLogo3 = new javax.swing.JLabel();
-        testerName3 = new javax.swing.JLabel();
-        testerLogo4 = new javax.swing.JLabel();
-        testerName4 = new javax.swing.JLabel();
-        TopPanel = new javax.swing.JPanel();
+        Dashlogo = new javax.swing.JLabel();
+        DashBoard = new javax.swing.JLabel();
+        ProjectLogo = new javax.swing.JLabel();
+        Project = new javax.swing.JLabel();
+        BugLogo = new javax.swing.JLabel();
+        Bugs = new javax.swing.JLabel();
+        LogoutLogo = new javax.swing.JLabel();
+        Logout = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         SystemName = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -49,35 +49,35 @@ public class Tester1 extends javax.swing.JFrame implements dataConnection {
         SidePanel.setBackground(new java.awt.Color(50, 50, 50));
         SidePanel.setPreferredSize(new java.awt.Dimension(340, 768));
 
-        testerName.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+        testerName.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
         testerName.setForeground(new java.awt.Color(150, 89, 165));
         testerName.setText("Tester Name");
 
         testerLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/person tester.png"))); // NOI18N
 
-        testerLogo1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/dashboard.png"))); // NOI18N
+        Dashlogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/dashboard.png"))); // NOI18N
 
-        testerName1.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
-        testerName1.setForeground(new java.awt.Color(109, 177, 147));
-        testerName1.setText("Dashboard");
+        DashBoard.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
+        DashBoard.setForeground(new java.awt.Color(109, 177, 147));
+        DashBoard.setText("Dashboard");
 
-        testerLogo2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/projects.png"))); // NOI18N
+        ProjectLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/projects.png"))); // NOI18N
 
-        testerName2.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
-        testerName2.setForeground(new java.awt.Color(109, 177, 147));
-        testerName2.setText("Projects");
+        Project.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
+        Project.setForeground(new java.awt.Color(109, 177, 147));
+        Project.setText("Projects");
 
-        testerLogo3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/bugs.png"))); // NOI18N
+        BugLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/bugs.png"))); // NOI18N
 
-        testerName3.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
-        testerName3.setForeground(new java.awt.Color(109, 177, 147));
-        testerName3.setText("Bugs");
+        Bugs.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
+        Bugs.setForeground(new java.awt.Color(109, 177, 147));
+        Bugs.setText("Bugs");
 
-        testerLogo4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/logout.png"))); // NOI18N
+        LogoutLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bugtrackingsystem/icons/images/logout.png"))); // NOI18N
 
-        testerName4.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
-        testerName4.setForeground(new java.awt.Color(109, 177, 147));
-        testerName4.setText("Logout");
+        Logout.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
+        Logout.setForeground(new java.awt.Color(109, 177, 147));
+        Logout.setText("Logout");
 
         javax.swing.GroupLayout SidePanelLayout = new javax.swing.GroupLayout(SidePanel);
         SidePanel.setLayout(SidePanelLayout);
@@ -93,24 +93,24 @@ public class Tester1 extends javax.swing.JFrame implements dataConnection {
                 .addGroup(SidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, SidePanelLayout.createSequentialGroup()
                         .addGap(77, 77, 77)
-                        .addComponent(testerLogo4)
+                        .addComponent(LogoutLogo)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(testerName4, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(Logout, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, SidePanelLayout.createSequentialGroup()
                         .addGap(35, 35, 35)
                         .addGroup(SidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, SidePanelLayout.createSequentialGroup()
-                                .addComponent(testerLogo2, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(ProjectLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(testerName2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addComponent(Project, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, SidePanelLayout.createSequentialGroup()
-                                .addComponent(testerLogo1)
+                                .addComponent(Dashlogo)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(testerName1, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(DashBoard, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, SidePanelLayout.createSequentialGroup()
-                                .addComponent(testerLogo3, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(BugLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(testerName3, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(Bugs, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         SidePanelLayout.setVerticalGroup(
@@ -122,43 +122,46 @@ public class Tester1 extends javax.swing.JFrame implements dataConnection {
                     .addComponent(testerName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(49, 49, 49)
                 .addGroup(SidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(testerLogo1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(testerName1, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(Dashlogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(DashBoard, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(70, 70, 70)
                 .addGroup(SidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(testerLogo2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(testerName2, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(ProjectLogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(Project, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(70, 70, 70)
                 .addGroup(SidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(testerLogo3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(testerName3, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(BugLogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(Bugs, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(82, 82, 82)
                 .addGroup(SidePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(testerLogo4, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(testerName4, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(LogoutLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Logout, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(110, Short.MAX_VALUE))
         );
 
-        TopPanel.setPreferredSize(new java.awt.Dimension(340, 62));
+        jPanel1.setBackground(new java.awt.Color(50, 50, 50));
+        jPanel1.setPreferredSize(new java.awt.Dimension(1026, 60));
 
         SystemName.setBackground(new java.awt.Color(207, 222, 231));
         SystemName.setFont(new java.awt.Font("Comic Sans MS", 1, 24)); // NOI18N
         SystemName.setForeground(new java.awt.Color(133, 89, 165));
         SystemName.setText("Bug Tracking System");
 
-        javax.swing.GroupLayout TopPanelLayout = new javax.swing.GroupLayout(TopPanel);
-        TopPanel.setLayout(TopPanelLayout);
-        TopPanelLayout.setHorizontalGroup(
-            TopPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(TopPanelLayout.createSequentialGroup()
-                .addGap(352, 352, 352)
-                .addComponent(SystemName))
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(212, 212, 212)
+                .addComponent(SystemName)
+                .addContainerGap(569, Short.MAX_VALUE))
         );
-        TopPanelLayout.setVerticalGroup(
-            TopPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(TopPanelLayout.createSequentialGroup()
-                .addGap(16, 16, 16)
-                .addComponent(SystemName))
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(SystemName)
+                .addContainerGap(14, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout backgroundPanelLayout = new javax.swing.GroupLayout(backgroundPanel);
@@ -168,14 +171,14 @@ public class Tester1 extends javax.swing.JFrame implements dataConnection {
             .addGroup(backgroundPanelLayout.createSequentialGroup()
                 .addComponent(SidePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(TopPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 1026, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         backgroundPanelLayout.setVerticalGroup(
             backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(backgroundPanelLayout.createSequentialGroup()
-                .addComponent(TopPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
             .addComponent(SidePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(backgroundPanelLayout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         getContentPane().add(backgroundPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -220,19 +223,19 @@ public class Tester1 extends javax.swing.JFrame implements dataConnection {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BugLogo;
+    private javax.swing.JLabel Bugs;
+    private javax.swing.JLabel DashBoard;
+    private javax.swing.JLabel Dashlogo;
+    private javax.swing.JLabel Logout;
+    private javax.swing.JLabel LogoutLogo;
+    private javax.swing.JLabel Project;
+    private javax.swing.JLabel ProjectLogo;
     private javax.swing.JPanel SidePanel;
     private javax.swing.JLabel SystemName;
-    private javax.swing.JPanel TopPanel;
     private javax.swing.JPanel backgroundPanel;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel testerLogo;
-    private javax.swing.JLabel testerLogo1;
-    private javax.swing.JLabel testerLogo2;
-    private javax.swing.JLabel testerLogo3;
-    private javax.swing.JLabel testerLogo4;
     private javax.swing.JLabel testerName;
-    private javax.swing.JLabel testerName1;
-    private javax.swing.JLabel testerName2;
-    private javax.swing.JLabel testerName3;
-    private javax.swing.JLabel testerName4;
     // End of variables declaration//GEN-END:variables
 }
